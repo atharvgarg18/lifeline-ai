3c64816c900ad20c78a8f80b5e647b361c678035
LifeLine AI Development Team <dev@lifeline.ai>
2026-05-30 19:08:31 +0530

    re

--- app/emergency/page.tsx
+++ app/emergency/page.tsx
@@ -1,4 +1,5 @@
 import EmergencySOSDashboard from "@/components/emergency/EmergencySOSDashboard";
+import SOSQuickRequest from "@/components/emergency/SOSQuickRequest";
 import LifelineMedicalSections from "@/components/emergency/LifelineMedicalSections";
 import LifelineDashboardSections from "@/components/emergency/LifelineDashboardSections";
 import { HealthAnalytics } from "@/components/emergency/HealthAnalytics";
@@ -6,6 +7,7 @@ import { HealthAnalytics } from "@/components/emergency/HealthAnalytics";
 export default function Page() {
   return (
     <>
+      <SOSQuickRequest />
       <EmergencySOSDashboard />
       <LifelineDashboardSections />
       <LifelineMedicalSections />
